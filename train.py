f8d3571971d0d7e0627fc58f7d3bd3da9f2a4205
SunJacques <jacques.sun@telecom-paris.fr>
2024-10-25 10:30:47 +0200

    v1

--- train.py
+++ train.py
@@ -5,11 +5,11 @@ import argparse
 from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.optim as optim
-
+from torchvision.utils import save_image, make_grid
 
 
 from model import Generator, Discriminator
-from utils import D_train, G_train, save_models, weights_init
+from utils import D_train, G_train, WD_train, WG_train, save_models, noise_generation
 
 
 
@@ -18,16 +18,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
     parser.add_argument("--epochs", type=int, default=100,
                         help="Number of epochs for training.")
-    parser.add_argument("--lr", type=float, default=0.0002,
+    parser.add_argument("--lr", type=float, default=0.0001,
                       help="The learning rate to use for training.")
     parser.add_argument("--batch_size", type=int, default=64, 
                         help="Size of mini-batches for SGD")
-    parser.add_argument("--device", type=str, default='cpu')
+    parser.add_argument("--mode", type=str, default='GAN')
 
     args = parser.parse_args()
 
 
-    os.makedirs('chekpoints', exist_ok=True)
+    os.makedirs('checkpoints', exist_ok=True)
     os.makedirs('data', exist_ok=True)
 
     # Data Pipeline
@@ -50,11 +50,8 @@ if __name__ == '__main__':
 
     print('Model Loading...')
     mnist_dim = 784
-    G = torch.nn.DataParallel(Generator(g_output_dim = mnist_dim)).to(args.device)
-    D = torch.nn.DataParallel(Discriminator(mnist_dim)).to(args.device)
-
-    G.apply(weights_init)
-    D.apply(weights_init)
+    G = Generator(g_output_dim = mnist_dim).cuda()
+    D = Discriminator(mnist_dim).cuda()
 
     # model = DataParallel(model).cuda()
     print('Model loaded.')
@@ -62,8 +59,10 @@ if __name__ == '__main__':
     
 
     # define optimizers
-    G_optimizer = optim.Adam(G.parameters(), lr = args.lr)
-    D_optimizer = optim.Adam(D.parameters(), lr = args.lr)
+    G_optimizer = optim.Adam(G.parameters(), lr = args.lr,betas=(0.5, 0.999))
+    D_optimizer = optim.Adam(D.parameters(), lr = args.lr,betas=(0.5, 0.999))
+    criterion = nn.BCELoss()
+    gpw = 10
 
     print('Start Training :')
     
@@ -71,15 +70,32 @@ if __name__ == '__main__':
     for epoch in trange(1, n_epoch+1, leave=True):           
         for batch_idx, (x, _) in enumerate(train_loader):
             x = x.view(-1, mnist_dim)
-            Dloss= D_train(args, x, G, D, D_optimizer)
-            Gloss = G_train(args, x, G, D, G_optimizer)
             
-        print(f'Epoch: {epoch}, D_loss: {Dloss}, G_loss: {Gloss}')
+            if args.mode == "WGAN":
+                if batch_idx % 2 == 0:
+                    Dloss, Dlipp = WD_train(x, G, D, D_optimizer, gpw)
+                
+                if batch_idx % 1 == 0:
+                    Gloss = WG_train(x, G, D, G_optimizer)
+                
+            else:
+                Dloss = D_train(x, G, D, D_optimizer, criterion)
+                Gloss = G_train(x, G, D, G_optimizer, criterion)
+                
+        if args.mode == "WGAN":
+            print(f'Epoch: {epoch}, D_loss: {Dloss}, G_loss: {Gloss}, D_lipp: {Dlipp}')
+        else:
+            print(f'Epoch: {epoch}, D_loss: {Dloss}, G_loss: {Gloss}')
             
-
+        # Generate images
+        with torch.no_grad():
+            z = noise_generation(16, 100)
+        grid = make_grid(G(z).view(-1, 1, 28, 28).cpu(), nrow=4, normalize=True)
+        print(f"predicted image class: {D(G(z)).argmax(1).detach().cpu().numpy()}")
+        save_image(grid, f'images/{args.mode}_{epoch}.png')
+            
         if epoch % 10 == 0:
             save_models(G, D, 'checkpoints')
-            
                 
     print('Training done')
 
--- utils.py
+++ utils.py
@@ -4,15 +4,10 @@ import torch.nn as nn
 from torchvision import transforms
 import torchvision
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
-        
-def lipconstant(args,D,x,y):
+def noise_generation(n, latent_dim):
+    return torch.randn(n, latent_dim).cuda()
+
+def lipconstant(D,x,y):
     b = x.shape[0]
     n = y.shape[0]
     # shrink vectors if they are too large
@@ -24,7 +19,7 @@ def lipconstant(args,D,x,y):
         b = n
     
     # compute interpolated points
-    alpha = torch.rand((b,1,1,1),device=args.device)
+    alpha = torch.rand((b,1),device='cuda')
     interp = alpha * y + (1 - alpha) * x
     interp.requires_grad_()
 
@@ -32,7 +27,7 @@ def lipconstant(args,D,x,y):
     Di = D(interp).view(-1)
 
     # Calculate gradients of probabilities with respect to examples
-    gradout = torch.ones(Di.size()).to(args.device)
+    gradout = torch.ones(Di.size()).cuda()
     gradients = torch.autograd.grad(outputs=Di, inputs=interp, grad_outputs=gradout,
        create_graph=True, retain_graph=True)[0]
 
@@ -43,77 +38,69 @@ def lipconstant(args,D,x,y):
     # Return gradient penalty
     return torch.mean(gradients_norm)
 
-def gradient_penalty(args,D,x,y):
-    b = x.shape[0]
-    n = y.shape[0]
-    # shrink vectors if they are too large
-    if n>b:
-        y = y[0:b,:]
-        n = b
-    else:
-        x = x[0:n,:]
-        b = n
-    # compute interpolated points
-    alpha = torch.rand((b,1,1,1)).to(args.device)
-    interp = (alpha * y + (1 - alpha) * x)
-    interp.requires_grad_()
+def compute_gradient_penalty(D, real_data, fake_data):
+    batch_size = real_data.size(0)
+    epsilon = torch.rand(batch_size, 1).cuda()
+    
+    # Interpolate between real and fake data
+    epsilon = epsilon.expand_as(real_data)
+    interpolated = epsilon * real_data + (1 - epsilon) * fake_data
+    interpolated = torch.autograd.Variable(interpolated, requires_grad=True).cuda()
+    
+    # Critic's prediction for interpolated data
+    interpolated_output = D(interpolated)
 
-    # Calculate probability of interpolated examples
-    Di = D(interp).view(-1)
-
-    # Calculate gradients of probabilities with respect to examples
-    gradout = torch.ones(Di.size()).to(args.device)
-    gradients = torch.autograd.grad(outputs=Di, inputs=interp, grad_outputs=gradout,
-       create_graph=True, retain_graph=True)[0]
-
-    # Derivatives of the gradient close to 0 can cause problems because of
-    # the square root, so manually calculate norm and add epsilon
-    gradients_norm = torch.sqrt(torch.sum(gradients ** 2, dim=1) + 1e-12)
-
-    # Return gradient penalty
-    return ((gradients_norm - 1) ** 2).mean()
+    # Compute gradients of D output w.r.t. interpolated data
+    gradients = torch.autograd.grad(outputs=interpolated_output, inputs=interpolated,
+                     grad_outputs=torch.ones(interpolated_output.size()).cuda(),
+                     create_graph=True, retain_graph=True, only_inputs=True)[0]
+    
+    # Compute the gradient penalty (||gradient||_2 - 1)^2
+    gradients = gradients.view(batch_size, -1)
+    gradient_norm = gradients.norm(2, dim=1)
+    gradient_penalty = ((gradient_norm - 1) ** 2).mean()
+    
+    return gradient_penalty
 
 
-def D_train(args,x, G, D, D_optimizer, gpw=0.1):
+def WD_train(x, G, D, D_optimizer, gpw=0.1):
     #=======================Train the discriminator=======================#
     D.zero_grad()
 
     # train discriminator on real
-    x_real, y_real = x, torch.ones(x.shape[0], 1)
-    x_real, y_real = x_real.to(args.device), y_real.to(args.device)
-
-    D_output = D(x_real)
-    D_real_loss = D_output.mean()
-    D_real_score = D_output
-
-    # train discriminator on facke
-    z = torch.randn(x.shape[0], 100).to(args.device)
-    x_fake, y_fake = G(z), torch.zeros(x.shape[0], 1).to(args.device)
-
-    D_output =  D(x_fake)
+    # x_real, y_real = x, torch.ones(x.shape[0], 1)
+    # x_real, y_real = x_real.cuda(), y_real.cuda()
     
-    D_fake_loss = D_output.mean()
-    D_fake_score = D_output
+    x_real = x.cuda()
+    z = noise_generation(x.shape[0], 100)
+    x_fake = G(z).detach().cuda()
+
+    real_output = D(x_real)
+    fake_output =  D(x_fake) 
+    
+    D_real_loss = real_output.mean()       
+    D_fake_loss = fake_output.mean()
+    # D_fake_score = D_output
 
     # gradient backprop & optimize ONLY D's parameters
-    D_loss = - D_real_loss + D_fake_loss + gpw * gradient_penalty(args, D, x_real, x_fake)
+    D_loss = D_fake_loss - D_real_loss + gpw * compute_gradient_penalty(D, x_real, x_fake)
     D_loss.backward()
     D_optimizer.step()
     
-    # Dlipp = lipconstant(args, D,x_real,x_fake)
+    Dlipp = lipconstant(D,x_real,x_fake)
         
-    return  D_loss.data.item()
+    return  D_loss.data.item(), Dlipp
 
 
-def G_train(args,x, G, D, G_optimizer):
+def WG_train(x, G, D, G_optimizer):
     #=======================Train the generator=======================#
     G.zero_grad()
 
-    z = torch.randn(x.shape[0], 100).to(args.device)
-    y = torch.ones(x.shape[0], 1).to(args.device)
+    z = noise_generation(x.shape[0], 100)
+    # y = torch.ones(x.shape[0], 1).cuda()
                  
-    G_output = G(z)
-    D_output = D(G_output)
+    fake = G(z)
+    D_output = D(fake)
     G_loss = - D_output.mean()
 
     # gradient backprop & optimize ONLY G's parameters
@@ -132,3 +119,49 @@ def load_model(G, folder):
     ckpt = torch.load(os.path.join(folder,'G.pth'))
     G.load_state_dict({k.replace('module.', ''): v for k, v in ckpt.items()})
     return G
+
+
+def D_train(x, G, D, D_optimizer, criterion):
+    D.zero_grad()
+
+    # train discriminator on real
+    x_real, y_real = x, torch.ones(x.shape[0], 1)
+    x_real, y_real = x_real.cuda(), y_real.cuda()
+
+    D_output = D(x_real)
+    D_real_loss = criterion(D_output, y_real)
+    D_real_score = D_output
+
+    # train discriminator on facke
+    z = torch.randn(x.shape[0], 100).cuda()
+    x_fake, y_fake = G(z), torch.zeros(x.shape[0], 1).cuda()
+
+    D_output =  D(x_fake)
+    
+    D_fake_loss = criterion(D_output, y_fake)
+    D_fake_score = D_output
+
+    # gradient backprop & optimize ONLY D's parameters
+    D_loss = D_real_loss + D_fake_loss
+    D_loss.backward()
+    D_optimizer.step()
+        
+    return  D_loss.data.item()
+
+
+def G_train(x, G, D, G_optimizer, criterion):
+    #=======================Train the generator=======================#
+    G.zero_grad()
+
+    z = torch.randn(x.shape[0], 100).cuda()
+    y = torch.ones(x.shape[0], 1).cuda()
+                 
+    G_output = G(z)
+    D_output = D(G_output)
+    G_loss = criterion(D_output, y)
+
+    # gradient backprop & optimize ONLY G's parameters
+    G_loss.backward()
+    G_optimizer.step()
+        
+    return G_loss.data.item()
